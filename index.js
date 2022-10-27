dd9d1d0a2fcb7bf7ec5711ee4aa9f68f7fbc0414
phchortolani <phchortolani@gmail.com>
2022-10-27 12:02:55 -0300

    testing 2

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import express from 'express'
+import Express from 'express'
 import { Obter, ObterPorData } from './js/obter.js'
-import cors from 'cors'
-
-const app = express()
-const port = process.env.PORT || 3000
+import Cors from 'cors'
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const app = Express()
+const port = process.env.PORT || 3000
+
+app.use(Express.static('public'))
+
+app.use(Express.json({ limit: '50mb' }));
 
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
-    app.use(cors());
+    app.use(Cors());
 
     next();
 });
 
 app.get('/', (req, res) => {
     if (!__dirname) return console.log("in get without dirname")
-    res.sendFile(__dirname + '/view/index.html');
+    res.sendFile(__dirname + '/public/view/index.html');
 })
 
 app.get('/Obter/:data', (req, res) => {
